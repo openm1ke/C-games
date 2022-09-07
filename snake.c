01f32923c9d5ce764df12f4beac5d5bc705bf699
Michael <muxaujish@gmail.com>
2022-09-07 23:23:43 +0300

    snake

--- snake.c
+++ snake.c
@@ -28,11 +28,13 @@ struct s_snake {
     int x;
     int y;
     int direction;
+    struct s_snake * next;
 };
 
 void draw_garden(struct s_apple *, struct s_snake *, int *, int *);
+struct s_snake * init_snake(struct s_snake *);
 void move_snake(struct s_snake *);
-struct s_snake draw_snake();
+//void draw_snake(struct s_snake *);
 struct s_apple draw_apple();
 
 
@@ -46,7 +48,16 @@ int main() {
     int scores = 0;
     int lives = 3;
 
-    struct s_snake snake = draw_snake();
+    struct s_snake snake[4];
+    struct s_snake * snake_head = init_snake(&snake[0]);
+    /*
+    printf("address %p - x = %d y = %d direction %d - next = %p\n", &snake[0], snake[0].x, snake[0].y, snake[0].direction, snake[0].next);
+    printf("address %p - x = %d y = %d direction %d - next = %p\n", &snake[1], snake[1].x, snake[1].y, snake[1].direction, snake[1].next);
+    printf("address %p - x = %d y = %d direction %d - next = %p\n", &snake[2], snake[2].x, snake[2].y, snake[2].direction, snake[2].next);
+    printf("address %p - x = %d y = %d direction %d - next = %p\n", &snake[3], snake[3].x, snake[3].y, snake[3].direction, snake[3].next);
+    exit(0);*/
+    //draw_snake(snake_head);
+
     struct s_apple apple = draw_apple();
 
     if (!isatty(STDIN_FILENO)) {
@@ -61,15 +72,15 @@ int main() {
             c = getchar();
             if (c == '=') time /= 2;
             if (c == '-') time *= 2;
-            if (c == 'd') snake.direction = 1;
-            if (c == 's') snake.direction = 2;
-            if (c == 'a') snake.direction = 3;
-            if (c == 'w') snake.direction = 4;
+            if (c == 'd') snake[0].direction = 1;
+            if (c == 's') snake[0].direction = 2;
+            if (c == 'a') snake[0].direction = 3;
+            if (c == 'w') snake[0].direction = 4;
             if (c == 'q') break;
         }
         system("clear");
-        move_snake(&snake);
-        draw_garden(&apple, &snake, &scores, &lives);
+        draw_garden(&apple, snake_head, &scores, &lives);
+        move_snake(snake_head);
         if (lives <= 0) break;
         usleep(time);
     }
@@ -79,31 +90,75 @@ int main() {
 }
 
 void move_snake(struct s_snake * snake) {
-    switch (snake->direction) {
-        case 1:
-            snake->x++;
-            break;
-        case 2:
-            snake->y++;
-            break;
-        case 3:
-            snake->x--;
-            break;
-        case 4:
-            snake->y--;
-            break;
-        default:
-            break;
+
+    struct s_snake * temp = snake;
+    while (temp != NULL) {
+        switch (temp->direction) {
+            case 1:
+                temp->x++;
+                break;
+            case 2:
+                temp->y++;
+                break;
+            case 3:
+                temp->x--;
+                break;
+            case 4:
+                temp->y--;
+                break;
+            default:
+                break;
+        }
+        temp = temp->next;
     }
+
+    temp = snake;
+    struct s_snake * head = temp;
+    temp = temp->next;
+    while (temp != NULL) {
+        struct s_snake * curr = temp;
+        if(curr->direction != head->direction) {
+            curr->direction = head->direction;
+            head = temp->next;
+        }
+
+        temp = temp->next;
+    }
+
 }
 
-struct s_snake draw_snake() {
-    struct s_snake snake;
-    snake.x = WIDTH / 4;
-    snake.y = HEIGHT - HEIGHT / 4;
-    snake.direction = 1;
+
+
+struct s_snake * init_snake(struct s_snake * snake) {
+    int d = 1;
+    int x = WIDTH / 4;
+    int y = HEIGHT - HEIGHT / 4;
+    snake[0].x = x;
+    snake[0].y = y;
+    snake[0].direction = d;
+    snake[0].next = &snake[1];
+
+    snake[1].x = x-1;
+    snake[1].y = y;
+    snake[1].direction = d;
+    snake[1].next = &snake[2];
+
+    snake[2].x = x-2;
+    snake[2].y = y;
+    snake[2].direction = d;
+    snake[2].next = &snake[3];
+
+    snake[3].x = x-3;
+    snake[3].y = y;
+    snake[3].direction = d;
+    snake[3].next = NULL;
+
     return snake;
 }
+/*
+void draw_snake(struct s_snake * snake) {
+
+}*/
 
 struct s_apple draw_apple() {
     struct s_apple apple;
@@ -116,22 +171,40 @@ struct s_apple draw_apple() {
 void draw_garden(struct s_apple * apple, struct s_snake * snake, int * scores, int * lives) {
     //printf("apple-x = %d apple-y %d\n", apple->x, apple->y);
     //printf("snake-x = %d snake-y %d\n", snake->x, snake->y);
+
+    printf("address %p - x = %d y = %d direction %d - next = %p\n", &snake[0], snake[0].x, snake[0].y, snake[0].direction, snake[0].next);
+    printf("address %p - x = %d y = %d direction %d - next = %p\n", &snake[1], snake[1].x, snake[1].y, snake[1].direction, snake[1].next);
+    printf("address %p - x = %d y = %d direction %d - next = %p\n", &snake[2], snake[2].x, snake[2].y, snake[2].direction, snake[2].next);
+    printf("address %p - x = %d y = %d direction %d - next = %p\n", &snake[3], snake[3].x, snake[3].y, snake[3].direction, snake[3].next);
+    /*exit(0);*/
+    struct s_snake * snk_temp;
+
     if (snake->x == apple->x && snake->y == apple->y) {
         *apple = draw_apple();
         *scores += 10;
     }
     if (snake->x == WIDTH - 1 || snake->x == 0 || snake->y == HEIGHT - 1 || snake->y == 0) {
-        *snake = draw_snake();
+        //*snake = draw_snake(snake);
         *lives -= 1;
     }
     printf("%s: %d\t%s: %d\n", SCORES, *scores, LIVES, *lives);
 
     for (int line = 0; line < HEIGHT; line++) {
         for (int column = 0; column < WIDTH; column++) {
+            snk_temp = snake;
+            while(snk_temp != NULL) {
+                if(line == snk_temp->y && column == snk_temp->x) {
+                    putchar(SNAKE);
+                    break;
+                }
+                snk_temp = snk_temp->next;
+            }
+            if(snk_temp) {
+                continue;
+            }
+
             if (line == apple->y && column == apple->x) {
                 putchar(APPLE);
-            } else if (line == snake->y && column == snake->x) {
-                putchar(SNAKE);
             } else if (line == 0 || line == HEIGHT - 1) {
                 putchar(WALL1);
             } else if (column == 0 || column == WIDTH - 1) {
