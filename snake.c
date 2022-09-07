cca331b9339f4b6a479b7983b5983beb3da9ecef
Michael <muxaujish@gmail.com>
2022-09-07 15:16:03 +0300

    snake

--- snake.c
+++ snake.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -12,6 +13,11 @@
 #define SPACE ' '
 #define APPLE 'o'
 #define NEWLINE '\n'
+#define SCORES "Scores"
+#define LIVES "Lives"
+#define GAMEOVER "Game over!"
+#define MIDSCREENX (WIDTH / 2) - strlen(GAMEOVER) / 2
+#define MIDSCREENY HEIGHT / 2
 
 struct s_apple {
     int x;
@@ -24,8 +30,7 @@ struct s_snake {
     int direction;
 };
 
-void draw_garden(struct s_apple *, struct s_snake *);
-void check_live(struct s_apple *, struct s_snake *);
+void draw_garden(struct s_apple *, struct s_snake *, int *, int *);
 void move_snake(struct s_snake *);
 struct s_snake draw_snake();
 struct s_apple draw_apple();
@@ -37,7 +42,10 @@ struct s_apple draw_apple();
 int main() {
     char c = ' ';
     int n = 0;
-    int time = 500000;
+    int time = 1000000;
+    int scores = 0;
+    int lives = 3;
+
     struct s_snake snake = draw_snake();
     struct s_apple apple = draw_apple();
 
@@ -51,8 +59,8 @@ int main() {
     while (c != 'q') {
         if (ioctl(0, FIONREAD, &n) == 0 && n > 0) {
             c = getchar();
-            if (c == '=') time /= 4;
-            if (c == '-') time *= 4;
+            if (c == '=') time /= 2;
+            if (c == '-') time *= 2;
             if (c == 'd') snake.direction = 1;
             if (c == 's') snake.direction = 2;
             if (c == 'a') snake.direction = 3;
@@ -61,23 +69,13 @@ int main() {
         }
         system("clear");
         move_snake(&snake);
-        check_live(&apple, &snake);
-        draw_garden(&apple, &snake);
-
+        draw_garden(&apple, &snake, &scores, &lives);
+        if (lives <= 0) break;
         usleep(time);
     }
 
 
     return 0;
-}
-
-void check_live(struct s_apple * apple, struct s_snake * snake) {
-    if (snake->x == apple->x && snake->y == apple->y) {
-        *apple = draw_apple();
-    }
-    if (snake->x == WIDTH - 1 || snake->x == 0 || snake->y == HEIGHT - 1 || snake->y == 0) {
-        *snake = draw_snake();
-    }
 }
 
 void move_snake(struct s_snake * snake) {
@@ -115,9 +113,19 @@ struct s_apple draw_apple() {
     return apple;
 }
 
-void draw_garden(struct s_apple * apple, struct s_snake * snake) {
+void draw_garden(struct s_apple * apple, struct s_snake * snake, int * scores, int * lives) {
     //printf("apple-x = %d apple-y %d\n", apple->x, apple->y);
     //printf("snake-x = %d snake-y %d\n", snake->x, snake->y);
+    if (snake->x == apple->x && snake->y == apple->y) {
+        *apple = draw_apple();
+        *scores += 10;
+    }
+    if (snake->x == WIDTH - 1 || snake->x == 0 || snake->y == HEIGHT - 1 || snake->y == 0) {
+        *snake = draw_snake();
+        *lives -= 1;
+    }
+    printf("%s: %d\t%s: %d\n", SCORES, *scores, LIVES, *lives);
+
     for (int line = 0; line < HEIGHT; line++) {
         for (int column = 0; column < WIDTH; column++) {
             if (line == apple->y && column == apple->x) {
@@ -128,6 +136,9 @@ void draw_garden(struct s_apple * apple, struct s_snake * snake) {
                 putchar(WALL1);
             } else if (column == 0 || column == WIDTH - 1) {
                 putchar(WALL2);
+            } else if (line == MIDSCREENY && column == MIDSCREENX && *lives <= 0) {
+                printf(GAMEOVER);
+                column += strlen(GAMEOVER) - 1;
             } else {
                 putchar(SPACE);
             }
