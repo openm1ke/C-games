89c27c847eabb2ecbedb09074def3aa54b9b14d8
Michael <muxaujish@gmail.com>
2022-09-07 12:00:01 +0300

    snake

--- snake.c
+++ snake.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <time.h>
+#include <unistd.h>
 
 #define WIDTH 80
 #define HEIGHT 25
@@ -23,6 +25,8 @@ struct s_snake {
 };
 
 void draw_garden(struct s_apple *, struct s_snake *);
+void check_live(struct s_apple *, struct s_snake *);
+void move_snake(struct s_snake *);
 struct s_snake draw_snake();
 struct s_apple draw_apple();
 
@@ -31,12 +35,68 @@ struct s_apple draw_apple();
 
 
 int main() {
-
+    char c = ' ';
+    int n = 0;
+    int time = 500000;
     struct s_snake snake = draw_snake();
     struct s_apple apple = draw_apple();
-    draw_garden(&apple, &snake);
+
+    if (!isatty(STDIN_FILENO)) {
+        if (freopen("/dev/tty", "r", stdin) == NULL) {
+            printf("Cannot open the file");
+        }
+    }
+    system("stty -icanon");
+
+    while (c != 'q') {
+        if (ioctl(0, FIONREAD, &n) == 0 && n > 0) {
+            c = getchar();
+            if (c == '=') time /= 4;
+            if (c == '-') time *= 4;
+            if (c == 'd') snake.direction = 1;
+            if (c == 's') snake.direction = 2;
+            if (c == 'a') snake.direction = 3;
+            if (c == 'w') snake.direction = 4;
+            if (c == 'q') break;
+        }
+        system("clear");
+        move_snake(&snake);
+        check_live(&apple, &snake);
+        draw_garden(&apple, &snake);
+
+        usleep(time);
+    }
+
 
     return 0;
+}
+
+void check_live(struct s_apple * apple, struct s_snake * snake) {
+    if (snake->x == apple->x && snake->y == apple->y) {
+        *apple = draw_apple();
+    }
+    if (snake->x == WIDTH - 1 || snake->x == 0 || snake->y == HEIGHT - 1 || snake->y == 0) {
+        *snake = draw_snake();
+    }
+}
+
+void move_snake(struct s_snake * snake) {
+    switch (snake->direction) {
+        case 1:
+            snake->x++;
+            break;
+        case 2:
+            snake->y++;
+            break;
+        case 3:
+            snake->x--;
+            break;
+        case 4:
+            snake->y--;
+            break;
+        default:
+            break;
+    }
 }
 
 struct s_snake draw_snake() {
@@ -57,7 +117,7 @@ struct s_apple draw_apple() {
 
 void draw_garden(struct s_apple * apple, struct s_snake * snake) {
     //printf("apple-x = %d apple-y %d\n", apple->x, apple->y);
-    printf("snake-x = %d snake-y %d\n", snake->x, snake->y);
+    //printf("snake-x = %d snake-y %d\n", snake->x, snake->y);
     for (int line = 0; line < HEIGHT; line++) {
         for (int column = 0; column < WIDTH; column++) {
             if (line == apple->y && column == apple->x) {
